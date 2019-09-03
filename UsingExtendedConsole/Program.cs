30dc6c8e7e4d17789d9dd95ab448967b078246bb
Zohar Peled <Peled.Zohar@gmail.com>
2019-09-03 14:14:04 +0300

    Update Program.cs

--- UsingExtendedConsole/Program.cs
+++ UsingExtendedConsole/Program.cs
@@ -55,6 +55,63 @@ namespace UsingExtendedConsole
             return result;
         }
 
+        private static int ActionsMenu()
+        {
+            return exConsole.Menu(
+                "Demonstraiting ExConsole - actions menu, clearWhenSelected = true",
+                true,
+                ("<c f='red'>Quit</c>", () => { /* a no-op */ }),
+                ("WriteLine", WriteLineMethods),
+                ("Read DateTime", ReadDateTimeMethods),
+                ("Read int", ReadIntMethods),
+                ("Clear lines", ClearLines),
+                ("Try it yourself", TryItYourself)
+            );
+        }
+
+        static void WriteLineMethods()
+        {
+            exConsole.WriteLine("Hello <c f='magenta'>world!</c>");
+            exConsole.WriteLine("Testing some <c b='darkRed'>unrelated</c> <xml att='val' at2='val2'>tags</xml>.");
+            exConsole.WriteLine("And another <tag> </tag> with no text in it");
+            exConsole.WriteLine("And another <tag>with <c f='yellow'>yellow</c> text in it</tag>.");
+        }
+		
+        private static void ReadDateTimeMethods()
+        {
+            var datetime = exConsole.ReadDateTime("Please enter a datetime value.", "Can't parse datetime value under the current culture settings.");
+            if (datetime.HasValue)
+            {
+                exConsole.WriteLine($"Value entered is <c f='green'>{ datetime.Value.ToShortDateString() }</c>.");
+            }
+            else
+            {
+                exConsole.WriteLine("User canceled.");
+            }
+        }
+
+        private static void ReadIntMethods()
+        {
+            var result = exConsole.ReadInt("Please enter an int value");
+            exConsole.WriteLine($"The int you selected is: <c f='green'>{result}</c>");
+
+            result = exConsole.ReadInt("Please enter an int between <c f='green'>0</c> and <c f='yellow'>5</c>, inclusive.", i => i >= 0 && i <= 5);
+            exConsole.WriteLine($"The int you selected is: <c f='green'>{result}</c>");
+        }
+
+        private static void ClearLines()
+        {
+            exConsole.WriteLine("Line <c f='white'>1</c>");
+            exConsole.WriteLine("Line <c f='gray'>2</c>");
+            exConsole.WriteLine("Line <c f='green'>3</c>");
+            exConsole.WriteLine("Line <c f='yellow'>4</c>");
+            exConsole.WriteLine("Line 5");
+            var result = exConsole.ReadInt("enter an int between 1 and 3 to clear lines", i => i >= 1 && i <= 3);
+            exConsole.ClearLastLines(result);
+            exConsole.Pause("Press a key to delete the last line.");
+            exConsole.ClearLastLine();
+        }
+
         private static void TryItYourself()
         {
             do
@@ -79,67 +136,5 @@ namespace UsingExtendedConsole
                 Console.WriteLine();
             } while(exConsole.ReadBool(ConsoleKey.Y, "Press <c f='green'>Y</c> to go again"));
         }
-
-        private static int ActionsMenu()
-        {
-            return exConsole.Menu(
-                "Demonstraiting ExConsole - actions menu, clearWhenSelected = true",
-                true,
-                ("<c f='red'>Quit</c>", () => { /* a no-op */ }),
-                ("WriteLine", WriteLineMethods),
-                ("Read DateTime", ReadDateTimeMethods),
-                ("Read int", ReadIntMethods),
-                ("Clear lines", ClearLines),
-                ("Try it yourself", TryItYourself)
-            );
-        }
-
-        private static void ClearLines()
-        {
-            exConsole.WriteLine("Line 1");
-            exConsole.WriteLine("Line 2");
-            exConsole.WriteLine("Line 3");
-            exConsole.WriteLine("Line 4");
-            exConsole.WriteLine("Line 5");
-            var result = exConsole.ReadInt("enter an int between 1 and 3 to clear lines", i => i >= 1 && i <= 3);
-            exConsole.ClearLastLines(result);
-            exConsole.Pause("Press a key to delete the last line.");
-            exConsole.ClearLastLine();
-        }
-
-        private static void ReadDateTimeMethods()
-        {
-            var datetime = exConsole.ReadDateTime("Please enter a datetime value.", "Can't parse datetime value under the current culture settings.");
-            if (datetime.HasValue)
-            {
-                exConsole.WriteLine($"Value entered is <c f='green'>{ datetime.Value.ToShortDateString() }</c>.");
-            }
-            else
-            {
-                exConsole.WriteLine("User canceled.");
-            }
-
-        }
-
-        private static void ReadIntMethods()
-        {
-            var result = exConsole.ReadInt("Please enter an int value");
-            exConsole.WriteLine($"The int you selected is: <c f='green'>{result}</c>");
-
-            result = exConsole.ReadInt("Please enter an int between <c f='green'>0</c> and <c f='yellow'>5</c>, inclusive.", i => i >= 0 && i <= 5);
-            exConsole.WriteLine($"The int you selected is: <c f='green'>{result}</c>");
-        }
-
-        static void WriteLineMethods()
-        {
-            exConsole.WriteLine("Hello <c f='magenta'>world!</c>");
-
-            exConsole.WriteLine("Testing some <c b='darkRed'>unrelated</c> <xml att='val' at2='val2'>tags</xml>.");
-
-            exConsole.WriteLine("And another <tag> </tag> with no text in it");
-
-            exConsole.WriteLine("And another <tag>with <c f='yellow'>yellow</c> text in it</tag>.");
-
-        }
-    }
+	}
 }
