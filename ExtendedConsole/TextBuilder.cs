25e0f2d3b66022ded7c73ab46151fcc24d9f96e2
Zohar Peled <Peled.Zohar@gmail.com>
2019-11-05 17:57:45 +0200

    Update TextBuilder.cs

--- ExtendedConsole/TextBuilder.cs
+++ ExtendedConsole/TextBuilder.cs
@@ -1,13 +1,19 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace ExtendedConsole
 {
+    /// <summary>
+    /// Builds the text and colors to write to Console.
+    /// </summary>
     internal class TextBuilder 
     {
         private readonly List<Action> _actions;
         private ConsoleColor _foreground, _background;
 
+        /// <summary>
+        /// Initialize a new instance of the TextBuilder class.
+        /// </summary>
         internal TextBuilder()
         {
             _actions = new List<Action>();
@@ -15,11 +21,20 @@ namespace ExtendedConsole
             _background = Console.BackgroundColor;
         }
 
+        /// <summary>
+        /// Adds text to write to the console.
+        /// </summary>
+        /// <param name="text">Text to write to the console.</param>
         internal void AddText(string text)
         {
             _actions.Add(() => Console.Write(text));
         }
 
+        /// <summary>
+        /// Adds a foreground color change, and returns the previous foreground color.
+        /// </summary>
+        /// <param name="color">A member of the ConsoleColor enum to set the foreground color to.</param>
+        /// <returns>A nullable member of the ConsoleColor enum.</returns>
         internal ConsoleColor? SetForegroundColor(ConsoleColor color)
         {
             var returnValue = _foreground;
@@ -28,6 +43,11 @@ namespace ExtendedConsole
             return returnValue;
         }
 
+        /// <summary>
+        /// Adds a background color change, and returns the previous background color.
+        /// </summary>
+        /// <param name="color">A member of the ConsoleColor enum to set the background color to.</param>
+        /// <returns>A nullable member of the ConsoleColor enum.</returns>
         internal ConsoleColor? SetBackgroundColor(ConsoleColor color)
         {
             var returnValue = _background;
@@ -36,6 +56,11 @@ namespace ExtendedConsole
             return returnValue;
         }
 
+        /// <summary>
+        /// Adds a foreground / background color change. If an argument is null, it has no effect.
+        /// </summary>
+        /// <param name="foreground">A nullable member of the ConsoleColor enum to set as foreground.</param>
+        /// <param name="background">A nullable member of the ConsoleColor enum to set as background.</param>
         internal void ResetColors(ConsoleColor? foreground, ConsoleColor? background)
         {
             if (foreground.HasValue)
@@ -50,6 +75,9 @@ namespace ExtendedConsole
             }
         }
 
+        /// <summary>
+        /// Writes the content of the TextBuilder to the Console and clears it.
+        /// </summary>
         internal void Write()
         {
             foreach (var action in _actions)
@@ -59,14 +87,13 @@ namespace ExtendedConsole
             _actions.Clear();
         }
 
+        /// <summary>
+        /// Writes the content of the TextBuilder to the Console, clears it and adds a new line.
+        /// </summary>
         internal void WriteLine()
         {
             _actions.Add(() => Console.WriteLine());
-            foreach (var action in _actions)
-            {
-                action();
-            }
-            _actions.Clear();
+            Write();
         }
 
     }
